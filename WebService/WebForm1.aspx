81e1fd5effb4d22b29a06ef7023d1ad8e1d67a2a
Henry Acevedo <brunokane7@yahoo.com>
2024-05-29 12:15:51 -0500

    Update 2024-05-29

---
+++ WebService/WebForm1.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebService.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <input id="cedula" name="filtro" type="radio" />
+            <label for="cedula">Cedula</label>
+
+            <input id="cedulaTextInput" name="cedula" type="text" runat="server" />
+        </div>
+        <div>
+            <input id="numcuenta" name="filtro" type="radio" />
+            <label for="numcuenta">Numero de Cuenta</label>
+
+            <input id="idProductoTextInput" name="idProducto" type="text" runat="server" />
+        </div>
+        <div>
+            <asp:Button ID="btnExecuteSP" runat="server" Text="Consultar (SP)" OnClick="btnExecuteSP_Click" />
+        </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"></asp:GridView>
+        </div>
+        <textbox id="texto1" runat="server"></textbox>
+    </form>
+</body>
+</html>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+
+
+<script>
+
+    //$(document).ready(function () {
+    //    $('#cedulaTextInput').hide();
+    //    $('#idProductoTextInput').hide();
+    //})
+
+
+    function HideInput()
+    {
+        console.log("funcion Hideinput");
+
+        var cedulaChecked = $('#cedula').is(':checked');
+        console.log("cedulaChecked:");
+        console.log(cedulaChecked);
+
+        if ($('#cedula').is(':checked'))
+        {
+            $('#idProductoTextInput').hide();
+            $('#cedulaTextInput').show();
+        }
+
+        if ($('#numcuenta').prop("checked"))
+        {
+            $('#cedulaTextInput').hide();
+            $('#idProductoTextInput').show();
+        }
+    }
+</script>
---
+++ WebService/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebService
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            texto1.InnerText = "Hola";
+        }
+
+        protected void btnExecuteSP_Click(object sender, EventArgs e)
+        {
+            //texto1.InnerText = "Hola2";
+            
+            string connectionString = "Data Source = LAPTOP-R601H3RA\\SQLEXPRESS; Initial Catalog=BGTest; Integrated Security=True";
+
+            string storedProcedureName;
+            //storedProcedureName = "AccountData";
+            storedProcedureName = "BGSP";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using(SqlCommand command = new SqlCommand(storedProcedureName, connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    var valorIdProductoAPasar = "";
+                    var valorCedulaAPasar = "";
+
+                    if(idProductoTextInput.Value == "")
+                    {
+                        //idProductoTextInput.Value = null;
+                        valorIdProductoAPasar = null;
+                    }
+                    else
+                    {
+                        valorIdProductoAPasar = idProductoTextInput.Value;
+                    }
+
+                    if(cedulaTextInput.Value == "")
+                    {
+                        //cedulaTextInput.Value = null;
+                        valorCedulaAPasar = null;
+                    }
+                    else
+                    {
+                        valorCedulaAPasar = cedulaTextInput.Value;
+                    }
+
+                    command.Parameters.AddWithValue("@AccountNum", idProductoTextInput.Value);
+                    //command.Parameters.AddWithValue("@AccountNum", valorIdProductoAPasar);
+
+                    command.Parameters.AddWithValue("@Cedula", cedulaTextInput.Value);
+                    //command.Parameters.AddWithValue("@Cedula", valorCedulaAPasar);
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                    DataSet dataset = new DataSet();
+
+                    adapter.Fill(dataset);
+
+                    GridView1.DataSource = dataset.Tables[0];
+                    GridView1.DataBind();
+
+                    //command.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
